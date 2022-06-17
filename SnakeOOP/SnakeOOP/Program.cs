5640c63e3448b162ee5f660fe0e70c351360337f
Andrey Sche <github@djfox.ru>
2022-06-17 11:20:29 +0300

    Update

--- SnakeOOP/SnakeOOP/Program.cs
+++ SnakeOOP/SnakeOOP/Program.cs
@@ -6,11 +6,30 @@ using System.Threading.Tasks;
 
 namespace SnakeOOP
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, Word");
+            InitVariables();
+
+            Console.ReadLine();
+        }
+
+        private static void InitVariables()
+        {
+            int x1 = 1, y1 = 3;
+            char sim1 = '*';
+            Draw(x1, y1, sim1);
+
+            x1 = 2; y1 = 4;
+            char sim2 = '#';
+            Draw(x1, y1, sim2);
+        }
+
+        private static void Draw(int x, int y, char sym)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
         }
     }
 }
